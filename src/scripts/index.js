e9009475018708440bd59fc82dfc738dc3f6bad6
mrsCandyBar <candicekbar@gmail.com>
2017-08-29 15:28:28 +0200

    setup base webpack project

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,26 @@
 import _ from 'lodash';
+import './styles.scss';
 
-// async function getComponent() {
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
 function component() {
     var element = document.createElement('div');
-    //const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
     var button = document.createElement('button');
-    var br = document.createEventObject('br');
+    var br = document.createElement('br');
 
     button.innerHTML = 'Click me and look at the console!';
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('test');
     element.appendChild(br);
     element.appendChild(button);
 
     button.onclick = e => import(/* webpackChunkName: "print"*/ './print').then(module => {
         var print = module.default;
         print();
-    })
+    });
     return element;
 }
 
-// getComponent().then(component => {
-//     document.body.appendChild(component);
-// })
 document.body.appendChild(component());

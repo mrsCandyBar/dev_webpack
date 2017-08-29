a9303d71b4c1f3b11e608ebdcccaf0b367a92bc4
mrsCandyBar <candicekbar@gmail.com>
2017-08-29 09:52:53 +0200

    webpack guide to LazyLoading

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import { cube } from './math.js';
+import _ from 'lodash';
 
-if (process.env.NODE_ENV !== 'production') {
-    console.log('Looks like we are in development mode!');
-}
-
+// async function getComponent() {
 function component() {
-    var element = document.createElement('pre');
+    var element = document.createElement('div');
+    //const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+    var button = document.createElement('button');
+    var br = document.createEventObject('br');
 
-    element.innerHTML = [
-        'Hello webpack!',
-        '5 cubed is equal to ' + cube(5)
-    ].join('\n\n');
+    button.innerHTML = 'Click me and look at the console!';
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.appendChild(br);
+    element.appendChild(button);
 
+    button.onclick = e => import(/* webpackChunkName: "print"*/ './print').then(module => {
+        var print = module.default;
+        print();
+    })
     return element;
 }
 
+// getComponent().then(component => {
+//     document.body.appendChild(component);
+// })
 document.body.appendChild(component());
